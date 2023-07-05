e0a110e2d59bf98e62fe74f492b88a77e40708c4
SempraETY <114128391+SempraETY@users.noreply.github.com>
2023-07-05 15:36:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 import streamlit as st
 import pypandoc
 import os
@@ -21,9 +22,16 @@ def pdf_to_html(filepath):
     subprocess.check_call(['docker', 'run', '-v', f'{os.getcwd()}:/pdf', 'bwits/pdf2htmlex', 'pdf2htmlEX', '--zoom', '1.3', docker_filepath, docker_html_filepath])
     return html_filepath
 
-def convert_to_md(filepath):
-    output = pypandoc.convert_file(filepath, 'html', 'markdown', outputfile="output.md")
-    assert output == ""
+def html_to_text(filepath):
+    with open(filepath, 'r', encoding='utf-8') as f:
+        contents = f.read()
+
+    soup = BeautifulSoup(contents, 'lxml')
+    text = soup.get_text()
+
+    with open(filepath.rsplit('.', 1)[0] + '.md', 'w', encoding='utf-8') as f:
+        f.write(text)
+
 
 def main():
     st.title("PDF Reader and Mind Map Generator")
@@ -40,7 +48,7 @@ def main():
             st.write(file_details)
             file_path = save_uploadedfile(uploaded_file)
             html_file_path = pdf_to_html(file_path)
-            convert_to_md(html_file_path)
+            html_to_text(html_file_path)
             st.write('文件已成功转换为 markdown 格式')
             # Display the PDF file
             st.markdown(f'<iframe src="{file_path}" width="700" height="800"></iframe>', unsafe_allow_html=True)
@@ -64,5 +72,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
